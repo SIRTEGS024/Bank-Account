fc18c88f41447c6ccaceca445fa754e28b6b2cfb
tega <kesienametitiri024@gmail.com>
2022-04-17 21:09:48 +0100

    add more functions in my script for my bank-account app

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,32 @@
 function Bank() {
-  this.accounts = [],
-  this.currentID = 0;
+  this.accounts = {},
+  this.currentId = 2209146835;
 }
+Bank.prototype.assignID = function () {
+  this.currentId += 1268;
+  return this.currentId;
+};
 Bank.prototype.addAccount = function(account) {
-  this.currentID += 1;
-  account.id = this.currentID;
-  this.accounts.push(account);
+  account.id = this.assignID();
+  this.accounts[account.id] = account
 }
-Bank.prototype.findAccount = function(id) {
-  var output = false;
-  for(let i=0; i<this.accounts.length; i++) {
-    if(this.accounts[i]) {
-      if(this.accounts[i].id === id) {
-        output = this.accounts[i];
-      }
-    }
+Bank.prototype.findAccount = function (id) {
+  if (this.accounts[id] != undefined) {
+    return this.accounts[id];
   }
-  return output;
-}
+  return false;
+};
 
-
-function Account(name, balance) {
+function account(name, balance) {
   this.name = name,
   this.balance = balance,
   this.history = [balance];
 }
-Account.prototype.makeDeposit = function(amount) {
+account.prototype.makeDeposit = function(amount) {
   this.balance += parseInt(amount);
   this.history.push("Credit:" + amount);
 }
-Account.prototype.makeWithdrawl = function(amount) {
+account.prototype.makeWithdrawl = function(amount) {
   if(amount > this.balance) {
     alert("Not enough funds! You only have: $" + this.balance);
   } else {
@@ -37,24 +34,46 @@ Account.prototype.makeWithdrawl = function(amount) {
     this.history.push("Debit:" + amount);
   }
 }
-Account.prototype.getHistory = function() {
+account.prototype.getHistory = function() {
   let output = "";
-  for(let i=0; i<this.history.length; i++) {
-    if(this.history[i].toString().includes("-")){
-      output += "<span class='negative'>$" + this.history[i] + "</span>";
+  Object.keys(this.history).forEach(function (key) {
+    if(this.history[key].toString().includes("Debit")){
+      output += "<span class='negative'>$" + this.history[key] + "</span>";
     } else {
-      output += "$" + this.history[i];
+      output += "$" + this.history[key];
     }
-    if(i < this.history.length-1) {
+    if(key < this.history.length-1) {
       output += ", ";
     }
-  }
+  });
   return output;
+}
+let bank = new Bank();
+
+function displayAccs(bankToDisplay){
+  let accList = $("#accSelect");
+  let accHTML = "";
+  Object.keys(bankToDisplay.accounts).forEach(function (key) {
+    const account = bankToDisplay.findAccount(key);
+    accHTML += "<option id=" + account.id + ">" + account.name + "</option>";
+  });
+  accList.html(accHTML);
+}
+function showAccount(accountId) {
+  const account = bank.findAccount(accountId);
+  $("##balanceDisp").show();
+  $("#accName").html(account.name);
+  $("#accNum").html(account.id);
+  $("#curBal").html("$" + account.balance);
+  $("#accHistory").html(account.getHistory());
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + account.id + ">Delete</button>");
 }
 
 
+
 $(document).ready(function() {
-  let bank = new Bank();
   $("input:radio[value=new]").click(function() {
       $("#current").hide();
       $("#new").fadeIn("slow");
